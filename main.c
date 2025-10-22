c4feb0415888aad1b6b307e87c635aafe6be2290
ViniciusKaique <viniciusloma@gmail.com>
2025-10-22 22:17:33 +0000

    ok

---
+++ main.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vinpache <vinpache@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 16:36:21 by vinpache          #+#    #+#             */
+/*   Updated: 2025/10/22 19:13:31 by vinpache         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_command	*cmd1;
+	t_command	*cmd2;
+
+	// char	*input;
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	// while (1)
+	// {
+	// 	input = readline("minishell$ ");
+	// 	if (!input)
+	// 	{
+	// 		printf("exit\n");
+	// 		break ;
+	// 	}
+	// 	if (*input)
+	// 		add_history(input);
+	// 	printf("Você digitou: %s\n", input);
+	// 	free(input);
+	// }
+	// return (0);
+	// t_command *cmd =  new_command();
+	// if(!cmd)
+	// 	return(1);
+	// cmd->args = (char *[]){"ls", "-la", NULL};
+	// printf("cmd: %s %s\n", cmd->args[0], cmd->args[1]);
+	// free(cmd);
+	// t_redirect *r = new_redirect(R_OUT_TRUNC, "saida.txt");
+	// printf("tipo: %d | arquivo: %s\n", r->type, r->file);
+	// free(r);
+
+	
+	cmd1 = new_command();
+	cmd2 = new_command();
+	cmd1->args = (char *[]){"ls", "-la", NULL};
+	cmd2->args = (char *[]){"grep", ".c", NULL};
+	add_command_back(&cmd1, cmd2);
+	printf("Primeiro comando: %s\n", cmd1->args[0]);
+	printf("Próximo comando: %s\n", cmd1->next->args[0]);
+}
